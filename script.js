587d5f5ed441abb76bae9662d79521064525b3d3
Sameer Kotecha <sameerzkotecha@gmail.com>
2023-02-01 19:16:53 -0600

    Create Popup and made background dim when clicked

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-const addBookBtn = document.getElementById("add-book")
+const addBookBtn = document.getElementById('add-book');
+const form = document.getElementById('form');
+const overlay = document.getElementById('overlay');
+const cancelBtn = document.getElementById('cnl-btn');
 
-document.addEventListener("click", e => {
-    console.log("hello")
+addBookBtn.addEventListener('click', e => {
+    form.classList.add('active');
+    overlay.classList.add('active');
 })
+
+cancelBtn.addEventListener('click', e => {
+    form.classList.remove('active');
+    overlay.classList.remove('active');
+})
+
