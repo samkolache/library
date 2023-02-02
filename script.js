966d873d6942c2861580dc3b2c496b4c5df74b38
Sameer Kotecha <sameerzkotecha@gmail.com>
2023-02-02 12:57:05 -0600

    Create DOM to show objects on page once fill out

--- script.js
+++ script.js
@@ -7,26 +7,54 @@ const author = document.getElementById('author');
 const pages = document.getElementById('pages');
 const submitBtn = document.getElementById('sbt-btn');
 const cancelBtn = document.getElementById('cnl-btn');
+const books = document.getElementById('books');
+let myLibrary = [];
 
 
-
-
-//open and Close forms
-addBookBtn.addEventListener('click', e => {
-    formContainer.classList.add('active');
-    overlay.classList.add('active');
-})
-
-cancelBtn.addEventListener('click', e => {
-    formContainer.classList.remove('active');
-    overlay.classList.remove('active');
-})
-
-//form validation
+//form validation and object creation
 form.addEventListener('submit', e => {
     e.preventDefault();
     formValidation();
+
+    //creation of a book object
+    let newBook = new Book(title.value, author.value, pages.value);
+    addBookToLibrary(newBook);
+
+    //to add books to library
+    function addBookToLibrary(book) {
+        myLibrary.push(book);
+        newBook = {};
+    }
+    //constructor
+    function Book(title, author, pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+    }
+    document.forms[0].reset();
+    console.log(myLibrary)
+    displayBooksOnPage();
 })
+
+function displayBooksOnPage() {
+
+    //loop over the library to make cards
+    myLibrary.forEach(myLibrary => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        books.appendChild(card);
+        for(let key in myLibrary) {
+            console.log(`${key}: ${myLibrary[key]}`)
+            const para = document.createElement('p');
+            para.textContent = (`${key}: ${myLibrary[key]}`);
+            card.appendChild(para);
+        }
+
+    })
+}
+
+
+
 
 function setError(element) {
     element.classList.add('error');
@@ -43,19 +71,19 @@ function formValidation() {
     const authorValue = author.value.trim();
     const pagesValue = pages.value.trim();
 
-    if(titleValue === "") {
+    if(titleValue === '') {
         setError(title);
     }else {
         setSuccess(title)
     }
 
-    if(authorValue === "") {
+    if(authorValue === '') {
         setError(author);
     }else {
         setSuccess(author)
     }
 
-    if(pagesValue === "") {
+    if(pagesValue === '') {
         setError(pages);
     }else {
         setSuccess(pages)
@@ -63,19 +91,14 @@ function formValidation() {
 
 }
 
+//open and Close forms
+addBookBtn.addEventListener('click', e => {
+    formContainer.classList.add('active');
+    overlay.classList.add('active');
+})
 
-//book storage and creation
-
-let myLibrary = [];
-
-function Book(title, author, pages) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-}
-
-
-function addBookToLibrary() {
-    //put books into array    
-}
+cancelBtn.addEventListener('click', e => {
+    formContainer.classList.remove('active');
+    overlay.classList.remove('active');
+})
 
