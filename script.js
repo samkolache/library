9d1420bf779c555b5e0de2bf07715e5affe5cd6f
Sameer Kotecha <sameerzkotecha@gmail.com>
2023-02-03 13:03:31 -0600

    Remove form validation, added requried attribute instead

--- script.js
+++ script.js
@@ -13,32 +13,38 @@ let myLibrary = [];
 let readAnswer = ''
 
 
-//form validation and object creation
+//when you submit the form do:
 form.addEventListener('submit', e => {
+    //prevent the form from resumbitting:
     e.preventDefault();
-    formValidation();
 
-    //creation of a book object
+    //check if the user checked the read button
     if(read.checked) {
         readAnswer = "Read"
     }else {
         readAnswer = "Not Read"
     }
-    let newBook = new Book(title.value, author.value, pages.value, readAnswer);
-    addBookToLibrary(newBook);
 
-    //to add books to library
-    function addBookToLibrary(book) {
-        myLibrary.push(book);
-        displayBooksOnPage();
-    }
-    //constructor
-    function Book(title, author, pages, read) {
+     //constructor
+     function Book(title, author, pages, read) {
         this.title = title;
         this.author = author;
         this.pages = pages;
         this.read = read;
     }
+
+    //creation of a Book object
+    let newBook = new Book(title.value, author.value, pages.value, readAnswer);
+
+    //Call function to push book into array
+    addBookToLibrary(newBook);
+
+    //function to push book and calls function to display the book
+    function addBookToLibrary(book) {
+        myLibrary.push(book);
+        displayBooksOnPage();
+    }
+   //reset the form
     document.forms[0].reset();
 })
 
@@ -52,11 +58,12 @@ function displayBooksOnPage() {
 
     //loop over the library to make cards
     let index = 0;
+    //Creates a card for each Book
     myLibrary.forEach(myLibrarys => {
         const card = document.createElement('div');
         card.classList.add('card');
         books.appendChild(card);
-
+        //Creates content for each card
         for(let key in myLibrarys) {
             const para = document.createElement('p');
             para.textContent = (`${myLibrarys[key]}`);
@@ -77,6 +84,7 @@ function displayBooksOnPage() {
          //event listener to remove button
          removeButton.addEventListener('click', removeBookFromLibrary)
 
+         //fucntion that deletes a card
          function removeBookFromLibrary() {
             let bookToDelete = removeButton.dataset.linkedArray;
             myLibrary.splice(parseInt(bookToDelete), 1);
@@ -87,43 +95,6 @@ function displayBooksOnPage() {
     })
 }
 
-
-
-
-function setError(element) {
-    element.classList.add('error');
-    element.classList.remove('success');
-}
-
-function setSuccess(element) {
-    element.classList.add('success');
-    element.classList.remove('error');
-}
-
-function formValidation() {
-    const titleValue = title.value.trim();
-    const authorValue = author.value.trim();
-    const pagesValue = pages.value.trim();
-
-    if(titleValue === '') {
-        setError(title);
-    }else {
-        setSuccess(title)
-    }
-
-    if(authorValue === '') {
-        setError(author);
-    }else {
-        setSuccess(author)
-    }
-
-    if(pagesValue === '') {
-        setError(pages);
-    }else {
-        setSuccess(pages)
-    }
-
-}
 
 //open and Close forms
 addBookBtn.addEventListener('click', e => {
